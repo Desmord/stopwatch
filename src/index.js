8d796251dd872a82e84c11564bb7768bba76d9d5
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-10-21 13:05:49 +0200

    Isntall sass and font-awesome. Add global styles.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import 'font-awesome/css/font-awesome.min.css';
+import './styles/normalize.scss';
+import './styles/global.scss';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
