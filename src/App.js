de2382adb2711f57c8eb675cb172e2b2ac87c5b0
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-10-21 13:42:41 +0200

    Create stopper stopwatch application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import { useState, useEffect } from "react";
+
+import Container from "./Container/Container";
+import TimeDisplay from "./TimeDisplay/TimeDisplay";
+import Button from "./Button/Button";
 
 function App() {
+  const [time, setTime] = useState(0); // in ms
+  const [timeCountdown, setTimeCountdown] = useState(null);
+
+  const start = () => {
+
+    setTimeCountdown(setInterval(() => {
+      setTime(prev => prev + 15);
+    }, 1))
+
+  }
+
+  const stop = () => {
+    if (timeCountdown) clearInterval(timeCountdown);
+  }
+
+  const reset = () => {
+    if (timeCountdown) clearInterval(timeCountdown);
+    setTime(0);
+  }
+
+  useEffect(() => {
+    return () => {
+      if (timeCountdown) clearInterval(timeCountdown);
+    }
+  }, [])
+
   return (
     <div>
-      witam
+      <Container>
+        <TimeDisplay time={time} />
+        <div>
+          <Button action={start}>Start</Button>
+          <Button action={stop}>Stop</Button>
+          <Button action={reset}>Reset</Button>
+        </div>
+      </Container>
     </div>
   );
 }
